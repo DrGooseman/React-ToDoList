1bcd0ecbc69b64f9184aac2831defb7b1a13b89f
DrGooseman <jamesjquinn12@gmail.com>
2020-02-20 23:45:44 +0300

    Adds Functionality
    You can type and add items to the to-do list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
 import React from "react";
+import Item from "./Item.jsx";
+import { useState } from "react";
+
+const items = ["An Item.", "An Item 2"];
 
 function App() {
+  const [textField, updateTextField] = useState("");
+  const [items, updateItems] = useState(["An Item.", "An Item 2"]);
+
+  function addItem() {
+    updateItems([...items, textField]);
+    updateTextField("");
+  }
+
+  function handleChange(event) {
+    updateTextField(event.target.value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={textField} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((item, i) => (
+            <Item key={i} name={item} />
+          ))}
         </ul>
       </div>
     </div>
